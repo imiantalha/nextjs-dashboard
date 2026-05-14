9e0f9b5268337a35b657f5d3ed2b1a4e19c55a01
Muhammad Talha <talhaarshad427@gmail.com>
2026-05-14 06:20:46 -0700

    Adding metadata to pages

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,7 +10,11 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton 
 } from '@/app/ui/skeletons';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'imiantalha\'s',
+};
 export default async function Page() {
 
   return (
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'imiantalha\'s Customers',
+};
 export default function Page() {
   return <p>Customers Page</p>;
 }
